1d1483da605a86b52f14eacebee3a4cd3d250f03
Raj8703 <sathwararaj124@gmail.com>
2026-06-16 08:51:12 +0530

    Add files via upload

---
+++ Saucedemo_Automation/src/test/java/com/Saucedemo/test/Login.java
@@ -0,0 +1,37 @@
+package com.Saucedemo.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Login {
+	public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("https://www.saucedemo.com/");
+
+        WebElement username = driver.findElement(By.id("user-name"));
+        WebElement password = driver.findElement(By.id("password"));
+        WebElement loginBtn = driver.findElement(By.id("login-button"));
+
+        username.sendKeys("standard_user");
+        password.sendKeys("secret_sauce");
+        loginBtn.click();
+
+        String currentUrl = driver.getCurrentUrl();
+
+        if(currentUrl.contains("inventory")) {
+            System.out.println("Login Successful");
+        } else {
+            System.out.println("Login Failed");
+        }
+
+        driver.quit();
+    }
+}
